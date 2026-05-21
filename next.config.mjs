057737d6a36b71b997f758efada629e8ef30e3eb
Fraaank <francis.padero@1.ustp.edu.ph>
2026-05-22 01:13:53 +0800

    misc: non-functional changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    qualities: [25, 50, 75, 100],
+  },
   // temporary fix: this will be a source of a problem I think says gemini
   serverExternalPackages: ["jspdf", "jspdf-autotable"],
 }
